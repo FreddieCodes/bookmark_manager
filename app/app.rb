771d9a72c2ef1fddf45bfdee621184209d2f2862
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-08-23 12:33:06 +0100

    Creating links

--- app/app.rb
+++ app/app.rb
@@ -6,16 +6,20 @@ class BookmarkManager < Sinatra::Base
 
 # set :root, File.dirname(__FILE__)
 
-  get '/' do
-
-    erb :index
-  end
-
   get '/links' do
     # This uses DataMapper's .all method to fetch all
     # data pertaining to this class from the database
     @links = Link.all
-    erb(:links)
+    erb(:'links/index')
+  end
+
+  get '/links/new' do
+  	erb(:'links/new')
+  end
+
+  post '/links' do
+  	Link.create(url: params[:url], title: params[:title])
+  	redirect '/links'
   end
 
 end
---
+++ spec/creating_link_spec.rb
@@ -0,0 +1,23 @@
+feature 'Links form' do
+
+  scenario 'I can see the form with the links' do
+  	visit '/links/new'
+  	expect(page).to have_field("url")
+  	expect(page).to have_field("title")
+  end
+
+  scenario 'I can see the Add button' do
+  	visit '/links'
+  	
+  end
+
+  scenario 'I can see my bookmarks' do
+  	visit '/links/new'
+  	fill_in("url", with: "http://www.google.co.uk")
+  	fill_in("title", with: "Google")
+  	click_button("Add")
+  	expect(current_path).to eq '/links'
+  	expect(page).to have_content("Title: Google")
+  end
+
+end
